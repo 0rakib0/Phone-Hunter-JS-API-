f798077b233dc2f4b5b8e257d5d0201be5049e1d
shathi <shathi@gmail.com>
2023-10-07 02:48:17 +0600

    banner section complate

--- Assinment-9/Event-Managemant/src/Pages/Home.jsx
+++ Assinment-9/Event-Managemant/src/Pages/Home.jsx
@@ -2,7 +2,37 @@
 const Home = () => {
     return (
         <div>
-            <h1>THis is Home Page</h1>
+            <div className="carousel w-full h-[80vh]">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://zoo-de-mack.com/wp-content/uploads/2018/02/slider_bg-2.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://zoo-de-mack.com/wp-content/uploads/2018/02/slider_bg-2.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://zoo-de-mack.com/wp-content/uploads/2018/02/slider_bg-2.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src="https://zoo-de-mack.com/wp-content/uploads/2018/02/slider_bg-2.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 };
